5cfc8396a6ce1968ada446b33b058042c17fe172
Emily Teixeira <emilyteixeira.student@careerdevs.com>
2019-02-14 03:18:25 +0000

    promise

--- async.js
+++ async.js
@@ -1,41 +1,47 @@
 window.onload = function(){
-
-function handleError(jqXHR, textStatus, error){
-    console.log(error);
-}
-
-$.ajax({
-    type: "GET",
-    url: "data/tweets.json",
-    success: cbTweets, 
-    error: handleError
-});
-
-function cbTweets(data){
-    console.log(data);
-        
-    $.ajax({
-    type: "GET",
-    url: "data/friends.json",
-    success: cbFriends,
-    error:handleError
-});
-
-}
-
-function cbFriends(data){
-        console.log(data);
     
-    $.ajax({
-    type: "GET",
-    url: "data/video.json",
-    success: function(data){
-        console.log(data);
-        
-    
-    },
-    error:handleError
+$.get("data/tweets.json").then(function(tweets){
+    console.log(tweets);
+    return $.get("data/friends.json");
+}).then(function(friends){
+    console.log(friends);
+    return $.get("data/videos.json");
+}).then(function(videos){
+    console.log(videos);
+})
+
+
+/*
+    function get(url){
+    return new Promise(function(resolve, reject){
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", url, true);
+        xhttp.onload= function(){
+            if(xhttp.status == 200){
+                resolve(JSON.parse(xhttp.response));
+            }else{
+                reject(xhttp.statusText);
+            }
+        };
+        xhttp.onerror = function(){
+            reject(xhttp.statusText);
+        };
+        xhttp.send();
     });
 }
+
+var promise = get("data/tweets.json");
+promise.then(function(tweets){
+    console.log(tweets);
+    return get("data/friends.json");
+}).then(function(friends){
+    console.log(friends);
+    return get("data/friends.json");
+}).then(function(videos){
+    console.log(videos);
+}).catch(function(error){
+    console.log(error);
+});
+
 };
-    
+/*
