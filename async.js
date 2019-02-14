32417a8ce338884317bccc1d48992b6b16349430
Emily Teixeira <emilyteixeira.student@careerdevs.com>
2019-02-14 03:08:02 +0000

    handleError

--- async.js
+++ async.js
@@ -1,29 +1,41 @@
 window.onload = function(){
-    
-  var http = new XMLHttpRequest();
-  
-  http.onreadystatechange = function(){
-    if(http.readyState == 4 && http.status == 200){
-        //console.log(JSON.parse(http.response));
-    }
-  };
-  
-  http.open("GET", "data/tweets.json", true);
-  http.send();
 
-//jquery method
-$.get("data/tweets.json", function(data){
-    console.log(data);
+function handleError(jqXHR, textStatus, error){
+    console.log(error);
+}
+
+$.ajax({
+    type: "GET",
+    url: "data/tweets.json",
+    success: cbTweets, 
+    error: handleError
 });
-console.log("test");
 
+function cbTweets(data){
+    console.log(data);
+        
+    $.ajax({
+    type: "GET",
+    url: "data/friends.json",
+    success: cbFriends,
+    error:handleError
+});
+
+}
+
+function cbFriends(data){
+        console.log(data);
+    
+    $.ajax({
+    type: "GET",
+    url: "data/video.json",
+    success: function(data){
+        console.log(data);
+        
+    
+    },
+    error:handleError
+    });
+}
 };
-
-// READY STATES
-
-//0- request not initialized
-//1- request has been set up
-//2- request has been sent
-//3- request is in process
-//4- request is complete
-
+    
