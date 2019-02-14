cde69a8a58da5b4f12b89cc77d1715507eb4194e
Emily Teixeira <emilyteixeira.student@careerdevs.com>
2019-02-14 03:25:44 +0000

    generator

--- async.js
+++ async.js
@@ -1,47 +1,30 @@
 window.onload = function(){
     
-$.get("data/tweets.json").then(function(tweets){
+ genWrap(function*(){
+     
+    var tweets = yield $.get("data/tweets/json");
     console.log(tweets);
-    return $.get("data/friends.json");
-}).then(function(friends){
+    var friends = yield $.get("data/friends/json");
     console.log(friends);
-    return $.get("data/videos.json");
-}).then(function(videos){
+    var videos = yield $.get("data/videos/json");
     console.log(videos);
-})
-
-
-/*
-    function get(url){
-    return new Promise(function(resolve, reject){
-        var xhttp = new XMLHttpRequest();
-        xhttp.open("GET", url, true);
-        xhttp.onload= function(){
-            if(xhttp.status == 200){
-                resolve(JSON.parse(xhttp.response));
-            }else{
-                reject(xhttp.statusText);
-            }
-        };
-        xhttp.onerror = function(){
-            reject(xhttp.statusText);
-        };
-        xhttp.send();
-    });
-}
-
-var promise = get("data/tweets.json");
-promise.then(function(tweets){
-    console.log(tweets);
-    return get("data/friends.json");
-}).then(function(friends){
-    console.log(friends);
-    return get("data/friends.json");
-}).then(function(videos){
-    console.log(videos);
-}).catch(function(error){
-    console.log(error);
-});
-
+     
+ });
+ 
+ function genWrap(generator){
+     
+    var gen = generator();
+     
+    function handle(yielded){
+        if(!yielded.done){
+            yielded.value.then(function(data){
+                return handle(gen.next(data));
+             });
+         }
+     }
+     
+     return handle(gen.next());
+     
+ }
+    
 };
-/*
